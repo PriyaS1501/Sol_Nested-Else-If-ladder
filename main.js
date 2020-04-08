8c58884ed915d8a3016eb30d33f913131055488f
PriyaS1501 <sawantpriyanka323@gmail.com>
2020-04-08 22:38:20 +0530

    nested if else

---
+++ main.js
@@ -0,0 +1,12 @@
+let marks = 50;
+let message = undefined;
+
+if (typeof(marks) === "number")
+{
+ message="it is number";
+}
+else{
+ message="Please enter number value only";
+}
+
+console.log(message); // true body print

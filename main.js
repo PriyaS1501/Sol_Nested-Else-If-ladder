1a5580c44d8d36dd94629327a17b922d2e72866f
PriyaS1501 <62501930+PriyaS1501@users.noreply.github.com>
2020-04-08 23:02:57 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,29 @@
-let marks = 50;
+let marks = 30;
 let message = undefined;
 
 if (typeof(marks) === "number")
 {
- message="it is number";
+  if (marks >= 0 && marks <= 100)
+  {
+    if(marks>=35 && marks<50){
+        message = "passed";
+    }
+    else if (marks>=50 && marks<60){
+        message= "second class";
+    }
+    else if (marks>=60 && marks<75){
+        message = "first class";
+    }
+    else if (marks>=75 && marks<=100){
+        message = "first class with distinction";
+    }
+    else {
+        message = "failed";
+    }
+  }
+  else {
+      message = "please enter marks between 0 to 100";
+  }
 }
 else{
  message="Please enter number value only";
